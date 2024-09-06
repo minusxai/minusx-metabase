9a99ea50dcf38b6339ec4c4553bbc7fd35af87aa
Arpit Saxena <arpit.tarang@gmail.com>
2024-09-05 20:33:35 -0700

    added testing tool

--- web/src/components/devtools/Testing.tsx
+++ web/src/components/devtools/Testing.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Box, VStack, Text, Stack, RadioGroup, Button,
-  HStack, Radio, Textarea } from '@chakra-ui/react';
+import {
+  Box, VStack, Text, Stack, RadioGroup, Button,
+  HStack, Radio, Textarea
+} from '@chakra-ui/react';
 import { uploadState } from '../../state/dispatch';
 import { BiCloudDownload, BiCloudUpload } from "react-icons/bi";
 import { useSelector } from 'react-redux';
 import { RootState } from '../../state/store';
-
+import { forwardToTab } from '../../app/rpc';
 
 export const Testing: React.FC<null> = () => {
   const rootState = useSelector((state: RootState) => state)
-  
+  const [text, setText] = React.useState("generate a random number and tell me what it is")
+  const [jupyterResponse, setJupyterResponse] = React.useState("")
+
   const downloadState = () => {
     const stateString = JSON.stringify(rootState, null, 2)
-    const blob = new Blob([stateString], {type: "application/json"})
+    const blob = new Blob([stateString], { type: "application/json" })
     const url = URL.createObjectURL(blob)
     const a = document.createElement('a')
     a.href = url
@@ -20,11 +24,11 @@ export const Testing: React.FC<null> = () => {
     a.click()
     a.remove()
   }
-  
+
   const uploadJson = (event: React.ChangeEvent<HTMLInputElement>) => {
     const fileReader = new FileReader()
     const file = event.target.files?.[0]
-    
+
     if (!file) {
       console.error('No file selected')
       return;
@@ -45,13 +49,14 @@ export const Testing: React.FC<null> = () => {
 
   return (
     <Box>
-        <Text fontSize="lg" fontWeight="bold">Testing Tools</Text>
-        <Box mt={4} backgroundColor="minusxBW.300" p={2} borderRadius={5}>
+      <Text fontSize="lg" fontWeight="bold">Testing Tools</Text>
+      <Box mt={4} backgroundColor="minusxBW.300" p={2} borderRadius={5}>
+        <VStack alignItems={"stretch"}>
           <HStack alignItems={"center"} marginTop={0} justifyContent={"space-between"}>
             <Text fontSize="sm">Redux State</Text>
             <HStack>
-              <Button size={"xs"} onClick={downloadState} colorScheme="minusxGreen"><BiCloudDownload size={20}/>Download</Button>
-              <Button size={"xs"} onClick={() => document.getElementById('file-input')?.click()} colorScheme="minusxGreen"><BiCloudUpload size={20}/>Upload</Button>
+              <Button size={"xs"} onClick={downloadState} colorScheme="minusxGreen"><BiCloudDownload size={20} />Download</Button>
+              <Button size={"xs"} onClick={() => document.getElementById('file-input')?.click()} colorScheme="minusxGreen"><BiCloudUpload size={20} />Upload</Button>
               <input
                 id="file-input"
                 type="file"
@@ -61,7 +66,17 @@ export const Testing: React.FC<null> = () => {
               />
             </HStack>
           </HStack>
-        </Box>
+          <HStack alignItems={"center"} marginTop={0} justifyContent={"space-between"}>
+            <Textarea value={text} onChange={(e) => setText(e.target.value)} />
+            <VStack>
+              <Button size={"xs"} onClick={() => forwardToTab("jupyter", text).then(res => {
+                setJupyterResponse(String((res as any)?.response))
+              })} colorScheme="minusxGreen">Call Jupyter</Button>
+              <span>{jupyterResponse}</span>
+            </VStack>
+          </HStack>
+        </VStack>
       </Box>
+    </Box>
   )
 }
