01eb26d1a00defa3eed50db14e2c5352b354ad3b
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2024-09-23 15:29:43 -0700

    Add UX to refresh membership status

--- web/src/components/common/Membership.tsx
+++ web/src/components/common/Membership.tsx
@@ -1,22 +1,51 @@
-import React, { useEffect } from 'react';
-import {
-    Button,
-    Text,
-  } from '@chakra-ui/react'
-import { update_profile } from '../../state/auth/reducer';
-import { dispatch } from '../../state/dispatch';
+import React, { useEffect } from 'react'
+import { Button, Text, VStack } from '@chakra-ui/react'
+import { update_profile } from '../../state/auth/reducer'
+import { dispatch } from '../../state/dispatch'
+import { configs } from '../../constants'
+import axios from 'axios'
+
+const url = `${configs.AUTH_BASE_URL}/profile`
+
+const refreshProfile = () => {
+  axios
+    .get(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(async (response) => {
+      const jsonResponse = await response.data
+      console.log('Profile response is', jsonResponse)
+      dispatch(
+        update_profile({
+          membership: jsonResponse.membership,
+          credits_expired: jsonResponse.credits_expired,
+        })
+      )
+    })
+}
+
+export const UpgradeMembershipButton = () => {
+  return (
+    <Button onClick={() => window.open('https://minusx.ai/pricing', '_blank')}>
+      Upgrade Membership
+    </Button>
+  )
+}
 
 export const MembershipBlock = () => {
-    useEffect(() => {
-        setTimeout(() => {
-            dispatch(update_profile({ credits_expired: false }))
-        }, 3000)
-    })
-    const finishPayment = () => {
+  useEffect(() => {
+    const interval = setInterval(refreshProfile, 3000)
+    return () => clearInterval(interval)
+  })
+  return <VStack>
+    <Text>Please upgrade your membership to continue</Text>
+    {/* <RefreshButton /> */}
+    <UpgradeMembershipButton />
+  </VStack>
+}
 
-    }
-    return <Text>
-        Blocked. Please pay to continue.
-        <Button onClick={finishPayment}>Pay</Button>
-    </Text>
+export const RefreshButton = () => {
+  return <Button onClick={refreshProfile}>Refresh</Button>
 }
