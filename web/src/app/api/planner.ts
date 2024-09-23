069e9f18911afb304876a960840277277cf3db4e
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2024-09-23 14:10:26 -0700

    On login, assign membership & credit status

--- web/src/app/api/planner.ts
+++ web/src/app/api/planner.ts
@@ -1,8 +1,9 @@
 
 import { configs } from '../../constants'
 import axios, { AxiosError } from 'axios';
-import { logoutState } from '../../state/dispatch';
+import { dispatch, logoutState } from '../../state/dispatch';
 import { toast } from '../toast';
+import { update_profile } from '../../state/auth/reducer';
 const url = `${configs.PLANNER_BASE_URL}/getLLMResponse`
 
 
@@ -28,6 +29,7 @@ export const getLLMResponse = async (payload: any, signal?: AbortSignal) => {
           })
           throw new Error("Unauthorized, please login again")
         } else if (error.response.status === 402) {
+          dispatch(update_profile({ credits_expired: true }))
           throw error
         } else {
           throw error
