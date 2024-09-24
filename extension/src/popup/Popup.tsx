951eb22b6e7f4fecc5662cf7b52a0a66a354ba53
Arpit Saxena <arpit.tarang@gmail.com>
2024-09-23 19:39:51 -0700

    wip

--- extension/src/popup/Popup.tsx
+++ extension/src/popup/Popup.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Box, Button, Text, ChakraProvider, Image, HStack, IconButton } from '@chakra-ui/react'
+import { Box, Button, Text, ChakraProvider, Image, HStack, IconButton, Link } from '@chakra-ui/react'
 import { BsTerminalFill, BsTools } from "react-icons/bs";
 import { TbWorldShare } from "react-icons/tb";
 import { get } from 'lodash'
@@ -9,18 +9,6 @@ const requestToolLink = 'https://minusx.ai/tool-request'
 const websiteLink = 'https://minusx.ai'
 
 export const Popup: React.FC = () => {
-  const defaultPopupText = `We currently support MinusX on any Jupyter (Lab, Notebook, Lite, etc), or Metabase (SQL Query page) instance. You can test-drive it in a playground we've created for you, or request support for your favourite analytics apps!`
-  const [popupText, setPopupText] = React.useState<string>(defaultPopupText);
-  useEffect(() => {
-    chrome.storage.local.get().then((localConfigs) => {
-      const popupText = get(localConfigs, "configs.popup_text")
-      if (popupText) {
-        setPopupText(popupText)
-      } else {
-        setPopupText(defaultPopupText)
-      }
-    })
-  })
   return (
     <ChakraProvider>
       <Box width="400px" p={5}>
@@ -28,7 +16,12 @@ export const Popup: React.FC = () => {
           <Image width={"150px"} src={chrome.runtime.getURL('logo.svg')} />
           <IconButton aria-label="Website" icon={<TbWorldShare />} onClick={() => window.open(websiteLink, '_blank')} isRound={true}/>
         </HStack>
-        <Text bg={'gray.50'} borderRadius={5} p={5} mt={2} fontSize="sm">{popupText}</Text>
+        <Text bg={'gray.50'} borderRadius={5} p={5} mt={2} fontSize="sm">
+        Supported tools: Jupyter (Lab, Notebook, Lite, etc), Metabase (SQL Query page), Posthog (HogQL page). 
+        <br/>
+        Don't see MinusX on your app? Send a <Link href="https://minusx.ai/privacy-simplified"
+            color="blue" onClick={() => console.log("hello")}>bug report</Link>.
+        </Text>
         <HStack justifyContent={"space-between"}>
           <Button mt={2} onClick={() =>  window.open(playgroundLink, '_blank')} aria-label="Go to Playground" leftIcon={<BsTerminalFill/>} colorScheme='blue' variant='solid'>Go to Playground</Button>
           <Button mt={3} onClick={() =>  window.open(requestToolLink, '_blank')} aria-label="Request Tool" leftIcon={<BsTools/>} colorScheme='gray' variant='solid'>App Support</Button>
