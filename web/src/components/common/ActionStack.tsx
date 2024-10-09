eee4e5c93ec6d2c3f0d7c4a3e2799b6a23c5e5f9
nuwandavek <vivekaithal44@gmail.com>
2024-10-09 10:33:55 -0700

    separate planning from other sections

--- web/src/components/common/ActionStack.tsx
+++ web/src/components/common/ActionStack.tsx
@@ -33,8 +33,6 @@ function extractMessageContent(input: string): string {
   return match ? match[1] : "";
 }
 
-const PLANNING_ACTIONS = ['Planning', 'Understanding App state', 'Thinking', 'Finalizing Actions', 'Validating Answers']
-
 export type ActionStatusView = Pick<Action, 'finished' | 'function' | 'status'>
 export const ActionStack: React.FC<{status: string, actions: Array<ActionStatusView>, index:number, content: string, latency: number}> = ({
   actions,
@@ -44,7 +42,6 @@ export const ActionStack: React.FC<{status: string, actions: Array<ActionStatusV
   latency
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
   const [controller, setController] = useState<any>(null);
   const currentTool = useSelector((state: RootState) => state.settings.iframeInfo.tool)
 
@@ -77,41 +74,22 @@ export const ActionStack: React.FC<{status: string, actions: Array<ActionStatusV
     return {text: null, code: null};
   }
   
-  let titles: string[] = [];
-  if (status == 'PLANNING') {
-    titles = PLANNING_ACTIONS
-  } else if (status == 'FINISHED') {
-    titles = actions.map(action => getActionLabels(action.function.name, 'labelDone'))    
-    titles = [[...new Set(titles)].join(', ')]
+  let title: string = "";
+  if (status == 'FINISHED') {
+    let titles = actions.map(action => getActionLabels(action.function.name, 'labelDone')) 
+    title = [...new Set(titles)].join(', ')
 
   } else {
-    titles = actions.map(action => getActionLabels(action.function.name, 'labelRunning'))
-    titles = [[...new Set(titles)].join(', ')]
+    let titles = actions.map(action => getActionLabels(action.function.name, 'labelRunning'))
+    title = [...new Set(titles)].join(', ')
 
   }
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
-    }, 3000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const scrollUp = keyframes`
-    from {
-      transform: translateY(100%);
-      opacity: 0;
-    }
-    to {
-      transform: translateY(0%);
-      opacity: 1;
-    }
-    `;
   const toggleExpand = () => {
     setIsExpanded(!isExpanded)
   }
   return (
-    <HStack aria-label={titles[currentTitleIndex]} className={'action-stack'} justifyContent={'start'} maxWidth={"100%"} width={status == 'PLANNING' ? "100%" : ""}> 
+    <HStack aria-label={title} className={'action-stack'} justifyContent={'start'} maxWidth={"100%"}> 
       <Box
         bg={'minusxGreen.800'}
         // bg={'minusxBW.600'}
@@ -124,7 +102,6 @@ export const ActionStack: React.FC<{status: string, actions: Array<ActionStatusV
         // color={'minusxGreen.800'}
         border={'1px'}
         maxWidth={'100%'}
-        width={status == 'PLANNING' ? "100%" : ""}
         position="relative"
       > 
         {content && <>
@@ -148,11 +125,11 @@ export const ActionStack: React.FC<{status: string, actions: Array<ActionStatusV
           <HStack>
             {isExpanded ? <BsChevronDown strokeWidth={1}/> : <BsChevronRight strokeWidth={1}/>}
             <Box flex={5}>
-              <Text key={currentTitleIndex} animation={ status === 'PLANNING' && currentTitleIndex > 0 ? `${scrollUp} 0.5s ease-in-out` : ""} >{titles[currentTitleIndex]}</Text>
+              <Text>{title}</Text>
             </Box>
-            { status != 'FINISHED' ? <Spinner size="xs" speed={'0.75s'} color="minusxBW.100" /> : null }
+            { status != 'FINISHED' ? <Spinner size="xs" speed={'0.75s'} color="minusxBW.100" mx={3} /> : null }
           </HStack>
-          { (status != 'PLANNING') && isExpanded ? <Text fontSize={"12px"} flexDirection={"row"} display={"flex"} justifyContent={"center"} alignItems={"center"}><MdOutlineTimer/>{latency}{"s"}</Text> : null }
+          { isExpanded ? <Text fontSize={"12px"} flexDirection={"row"} display={"flex"} justifyContent={"center"} alignItems={"center"}><MdOutlineTimer/>{latency}{"s"}</Text> : null }
           
         </HStack>
         {isExpanded && actions.map((action, index) => {
@@ -206,7 +183,7 @@ export const OngoingActionStack: React.FC = () => {
     return null
   }
   else if (activeThread.status == 'PLANNING') {
-    return <ActionStack actions={[]} status={activeThread.status} index={-1} content='' latency={0}/>
+    return <PlanningActionStack/>
   } 
   else {
     const messages = activeThread.messages
@@ -228,3 +205,44 @@ export const OngoingActionStack: React.FC = () => {
     return <ActionStack actions={actions} content={actionPlan.content.messageContent} status={activeThread.status} index={-1} latency={0}/>
   }
 }
+
+const scrollUp = keyframes`
+  from {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+  `;
+
+const PlanningActionStack: React.FC = () => {
+  const planningActions = ['Planning', 'Understanding App state', 'Thinking', 'Finalizing Actions', 'Validating Answers']
+  const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTitleIndex((prevIndex) => (prevIndex + 1) % planningActions.length);
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+  <HStack aria-label={"planning"} className={'action-stack'} justifyContent={'start'} width={"100%"}> 
+    <Box
+      bg={'minusxGreen.800'}
+      p={2}
+      borderRadius={5}
+      color={'minusxBW.50'}
+      width={"100%"}
+      position="relative"
+    >
+      <HStack>
+        <Box>
+          <Text key={currentTitleIndex} animation={currentTitleIndex > 0 ? `${scrollUp} 0.5s ease-in-out` : ""} >{planningActions[currentTitleIndex]}</Text>
+        </Box>
+        <Spinner size="xs" speed={'0.75s'} color="minusxBW.100" />
+      </HStack>
+    </Box>
+  </HStack>
+)}
