7b6da1df5ea190442e8c63b5d06a2d9958fb3a70
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2024-11-23 13:56:29 -0800

    Add web side native Event Listeners

--- web/src/helpers/nativeElements.ts
+++ web/src/helpers/nativeElements.ts
@@ -3,10 +3,22 @@ import { attachNativeElementsListener } from "../app/rpc"
 
 type EventListener = (event: string) => void
 
+interface NativeEventPayload {
+  eventID: number
+  event: string
+}
+
 const listeners: Record<number, EventListener> = {}
 
 export const addNativeEventListener = async (selector: QuerySelector, listener: EventListener, events: string[]=['click']) => {
   const eventID = await attachNativeElementsListener(selector, events)
+  listeners[eventID] = listener
 }
 
-export const onNativeEvent = (eventID: number, event: string) => {}
+export const onNativeEvent = (payload: NativeEventPayload) => {
+  const { eventID, event } = payload
+  if (!(eventID in listeners)) {
+    return
+  }
+  listeners[eventID](event)
+}
