07bab0d164f6c0413c3ba4ae5b62600517b11e23
nuwandavek <vivekaithal44@gmail.com>
2025-04-08 15:38:30 -0700

    looks nice

--- web/src/components/common/TablesContext.tsx
+++ web/src/components/common/TablesContext.tsx
@@ -1,10 +1,8 @@
 import React from "react"
 import { FilteredTable } from '../common/FilterableTable';
-import { FormattedTable, MetabaseContext } from 'apps/types';
+import { MetabaseContext } from 'apps/types';
 import { getApp } from '../../helpers/app';
-import { getParsedIframeInfo } from "../../helpers/origin"
-import { isEmpty } from 'lodash';
-import { Text, Box, Badge, Link, Select} from "@chakra-ui/react";
+import { Text, Link, HStack} from "@chakra-ui/react";
 import { applyTableDiff, TableInfo, setSelectedContext } from "../../state/settings/reducer";
 import { dispatch, } from '../../state/dispatch';
 import { useSelector } from 'react-redux';
@@ -18,10 +16,6 @@ export const TablesContext: React.FC<null> = () => {
   const toolContext: MetabaseContext = useAppStore((state) => state.toolContext)
   const tableDiff = useSelector((state: RootState) => state.settings.tableDiff)
 
-  const tool = getParsedIframeInfo().tool
-  if (tool != 'metabase' || isEmpty(toolContext)) {
-    return <Text>Coming soon!</Text>
-  }
   const relevantTables = toolContext.relevantTables || []
   const dbInfo = toolContext.dbInfo
   const allTables = dbInfo?.tables || []
@@ -43,17 +37,9 @@ export const TablesContext: React.FC<null> = () => {
   }
   
   return <>
-    <Text fontSize="lg" fontWeight="bold">Tables</Text>
+    <HStack w={"100%"} justify={"space-between"}><Text fontSize="md" fontWeight="bold">Tables</Text><Text fontSize="sm" color={"minusxGreen.600"} textAlign={"right"}>[{updatedRelevantTables.length} out of {allTables.length} tables selected]</Text></HStack>
     <Text color={"minusxBW.600"} fontSize="sm">The selected tables are in MinusX context while answering queries. You can select/unselect tables to control the context.</Text>
     <Text fontSize="sm" color={"minusxGreen.600"} mt={1}><Link width={"100%"} textAlign={"center"} textDecoration={"underline"} href="https://docs.minusx.ai/en/articles/10501728-modify-relevant-tables-list" isExternal>Read more about table context.</Link></Text>
-
-    <Box mt={2} mb={2}>
-    <Text fontWeight="bold">DB Info</Text>
-    <Text fontSize="sm"><Text as="span">DB Name: <Badge color={"minusxGreen.600"}>{dbInfo.name}</Badge></Text></Text>
-    <Text fontSize="sm"><Text as="span">DB Description: {dbInfo.description || "-"}</Text></Text>
-    <Text fontSize="sm"><Text as="span">SQL Dialect: </Text><Badge color={"minusxGreen.600"}>{dbInfo.dialect}</Badge></Text>
-    </Box>
-    <Text fontSize="sm" color={"minusxGreen.600"} textAlign={"right"} mt={2}>{updatedRelevantTables.length} out of {allTables.length} tables selected</Text>
     <FilteredTable dbId={dbInfo.id} data={allTables} selectedData={updatedRelevantTables} addFn={updateAddTables} removeFn={updateRemoveTables}/>
   </>
 }
--- web/src/components/devtools/Context.tsx
+++ web/src/components/devtools/Context.tsx
@@ -1,25 +1,47 @@
 import React from "react"
 import { TablesContext } from '../common/TablesContext';
-import { FormattedTable, MetabaseContext } from 'apps/types';
 import { getApp } from '../../helpers/app';
-import { getParsedIframeInfo } from "../../helpers/origin"
-import { isEmpty } from 'lodash';
-import { Text, Box, Badge, Link, Select} from "@chakra-ui/react";
-import { applyTableDiff, TableInfo, setSelectedContext } from "../../state/settings/reducer";
+import { Text, Badge, Select, Spacer, Box} from "@chakra-ui/react";
+import { setSelectedContext } from "../../state/settings/reducer";
 import { dispatch, } from '../../state/dispatch';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../state/store';
-import { applyTableDiffs } from "apps";
+import { getParsedIframeInfo } from "../../helpers/origin"
+import { isEmpty } from 'lodash';
+import { MetabaseContext } from 'apps/types';
+
+
 
 const useAppStore = getApp().useStore()
 
 
 
 export const Context: React.FC<null> = () => {
+    const toolContext: MetabaseContext = useAppStore((state) => state.toolContext)
+    const tableDiff = useSelector((state: RootState) => state.settings.tableDiff)
+
+    const tool = getParsedIframeInfo().tool
+    if (tool != 'metabase' || isEmpty(toolContext)) {
+    return <Text>Coming soon!</Text>
+    }
+    const relevantTables = toolContext.relevantTables || []
+    const dbInfo = toolContext.dbInfo
+    const allTables = dbInfo?.tables || []
+    
+      
     const selectedContext = useSelector((state: RootState) => state.settings.selectedContext)
     const availableContexts = useSelector((state: RootState) => state.settings.availableContexts)
     return <>
         <Text fontSize="lg" fontWeight="bold">Context</Text>
+        <Box mt={2} mb={2}>
+            <Text fontWeight="bold">DB Info</Text>
+            <Text fontSize="sm"><Text as="span">DB Name: <Badge color={"minusxGreen.600"}>{dbInfo.name}</Badge></Text></Text>
+            <Text fontSize="sm"><Text as="span">DB Description: {dbInfo.description || "-"}</Text></Text>
+            <Text fontSize="sm"><Text as="span">SQL Dialect: </Text><Badge color={"minusxGreen.600"}>{dbInfo.dialect}</Badge></Text>
+        </Box>
+            
+        <Spacer height={5}/>
+        <Text fontSize="md" fontWeight="bold">Available Contexts</Text>
         <Select placeholder="Select a context" mt={2} colorScheme="minusxGreen" value={selectedContext} onChange={(e) => {dispatch(setSelectedContext(e.target.value))}}>
             {
                 availableContexts.map((context: any) => {
@@ -27,6 +49,7 @@ export const Context: React.FC<null> = () => {
                 })
             }
         </Select>
+        <Spacer height={5}/>
         {
             selectedContext === "tables" && <TablesContext />
         }
