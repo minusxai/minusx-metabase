024247705fddab01b35404ec40995461bcab9532
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-05-17 10:11:27 +0530

    Enable MinusX on dashboards

--- apps/src/metabase/appState.ts
+++ apps/src/metabase/appState.ts
@@ -153,7 +153,6 @@ export class MetabaseState extends DefaultAppState<MetabaseAppState> {
 }
 
 function shouldEnable(elements: DOMQueryMapResponse, url: string) {
-  const appSettings = RPCs.getAppSettings()
   const hash = btoa(JSON.stringify({
         "dataset_query": {
             "database": null,
@@ -171,18 +170,10 @@ function shouldEnable(elements: DOMQueryMapResponse, url: string) {
   const SQLQueryURL = new URL(url).origin + '/question#' + hash;
   const reason = `To enable MinusX on Metabase, head over to the SQL query [page](${SQLQueryURL})!`
   if (isDashboardPageUrl(url)) {
-    if (appSettings.drMode) {
-        return {
-        value: true,
-        reason: "",
-        };
-    }
-    else{
-        return {
-            value: false,
-            reason: reason,
-        };
-    }
+    return {
+      value: true,
+      reason: "",
+    };
   }
   if (isEmpty(elements.editor)) {
     return {
