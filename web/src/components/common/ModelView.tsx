d487add75f1516266015bd396b25855dddc9efcb
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-05-28 03:25:32 +0530

    Extract out fetchTableWithColumns for model view

--- web/src/components/common/ModelView.tsx
+++ web/src/components/common/ModelView.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Box, Text } from '@chakra-ui/react';
 import { CodeBlock } from './CodeBlock'
 import { getApp } from '../../helpers/app';
 import { FormattedTable, MetabaseContext } from 'apps/types';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../state/store';
 import { load, dump } from 'js-yaml';
-import { getTableContextYAML } from 'apps';
+import { getTableContextYAML, memoizedFetchTableData } from 'apps';
 import { createSchemaFromDataModel } from '../../helpers/catalog';
 import { isEmpty } from 'lodash';
 
@@ -20,7 +20,26 @@ const useAppStore = getApp().useStore()
 export const ModelView: React.FC<ModelViewProps> = ({ yamlContent, tables }) => {
   const toolContext: MetabaseContext = useAppStore((state) => state.toolContext)
   const drMode = useSelector((state: RootState) => state.settings.drMode);
-  const relevantTables = toolContext.relevantTables || []
+  const relevantTables = tables ? tables : toolContext.relevantTables || []
+  const [isLoading, setIsLoading] = useState(true)
+  const [loadedTables, setLoadedTables] = useState<FormattedTable[]>([])
+
+  useEffect(() => {
+    Promise.all(relevantTables.map(table => memoizedFetchTableData(table.id))).then((tableInfos) => {
+      setIsLoading(false)
+      const loadedTableInfos = tableInfos.filter(tableInfo => tableInfo != "missing")
+      setLoadedTables(loadedTableInfos)
+    }).catch(() => {
+      setIsLoading(false)
+    });
+  }, [relevantTables])
+
+  if (isLoading) {
+    return (
+      <Text>Loading...</Text>
+    )
+  }
+
   let entityJSON
   if (!tables) {
     let yamlContentJSON
@@ -29,9 +48,9 @@ export const ModelView: React.FC<ModelViewProps> = ({ yamlContent, tables }) => 
     } catch (e) {
       console.error('Invalid YAML content:', e);
     }
-    entityJSON = getTableContextYAML(relevantTables, yamlContentJSON, drMode) || {};
+    entityJSON = getTableContextYAML(loadedTables, yamlContentJSON, drMode) || {};
   } else {
-    entityJSON = getTableContextYAML(tables, undefined, drMode) || {};
+    entityJSON = getTableContextYAML(loadedTables, undefined, drMode) || {};
   }
   const modelViewSchema = dump(createSchemaFromDataModel(entityJSON));
   return (
