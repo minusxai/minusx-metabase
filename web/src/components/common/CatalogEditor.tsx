1178289f2b87561118817324cf565c3f53db9a44
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-05-27 23:48:05 +0530

    Move monaco yaml schema to editor file

--- web/src/components/common/CatalogEditor.tsx
+++ web/src/components/common/CatalogEditor.tsx
@@ -14,6 +14,30 @@ import { getParsedIframeInfo } from "../../helpers/origin";
 import Editor from '@monaco-editor/react';
 import { CodeBlock } from "./CodeBlock";
 import { ModelView } from "./ModelView";
+import * as monaco from 'monaco-editor';
+import { configureMonacoYaml } from 'monaco-yaml';
+
+configureMonacoYaml(monaco, {
+  enableSchemaRequest: true,
+  hover: true,
+  completion: true,
+  validate: true,
+  format: true,
+  schemas: [
+    {
+      uri: 'http://myserver/schema.json', // Unique URI for the schema
+      fileMatch: ['*'], // Apply to all YAML files
+      schema: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          version: { type: 'string' }
+        },
+        required: ['name']
+      }
+    }
+  ]
+});
 
 const useAppStore = getApp().useStore()
 
