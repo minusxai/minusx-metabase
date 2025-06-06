c30ce38210142a5f152138cf832d4de1062f250b
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-06-06 16:38:22 +0530

    Add auth check for addUserMessage

--- web/src/chat/chat.ts
+++ web/src/chat/chat.ts
@@ -6,9 +6,22 @@ import { updateCredits } from '../state/billing/reducer'
 import { getApp } from '../helpers/app'
 import { AppState } from 'apps/types'
 import { getState } from '../state/store'
+import { toast } from '../app/toast'
 
 export default {
-  async addUserMessage({ content }: { content: DefaultMessageContent }) {  
+  async addUserMessage({ content }: { content: DefaultMessageContent }) {
+    const state = getState()
+    if (!state.auth.is_authenticated) {
+      toast({
+        title: 'Log in to start using this feature!',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-right',
+      })
+      return
+    }
+    
     dispatch(
       addUserMessage({
         content,
@@ -17,7 +30,7 @@ export default {
     )
     const app = getApp()
     const appState = await app.getState() as AppState
-    const messageId = getState().chat.activeThread
+    const messageId = state.chat.activeThread
     app.setCachedState(messageId, appState)
   },
   addErrorMessage(err: string) {
