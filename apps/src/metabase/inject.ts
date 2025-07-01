a7a865fc104dbb8864407c94edadd6f6b6dafad9
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-07-01 12:49:59 +0530

    Add metabase state subscribe RPCs

--- apps/src/metabase/inject.ts
+++ apps/src/metabase/inject.ts
@@ -1,4 +1,4 @@
-import { get, isEmpty } from "lodash"
+import { get, isEmpty, set } from "lodash"
 import { initWindowListener } from 'extension'
 
 const getMetabaseState = (path: Parameters<typeof get>[1]) => {
@@ -35,6 +35,47 @@ const dispatchMetabaseAction = (type: string, payload: any) => {
             payload
         })
     }
+}
+
+interface EventListener {
+    event: string
+    path: Parameters<typeof get>[1]
+}
+
+const eventListeners: Array<EventListener> = []
+
+async function onMetabaseLoad() {
+    while (true) {
+        const store = get(window, 'Metabase.store') as any
+        if (!store || !store.getState) {
+            await new Promise(resolve => setTimeout(resolve, 500))
+        } else {
+            break
+        }
+    }
+    const store = get(window, 'Metabase.store') as any
+    let oldState = store.getState()
+    store.subscribe(() => {
+        const state = store.getState()
+        eventListeners.forEach(({ event, path }) => {
+            const oldValue = get(oldState, path)
+            const newValue = get(state, path)
+            if (oldValue !== newValue) {
+                set(oldState, path, newValue)
+                sendIFrameMessage({
+                    key: 'metabaseStateChange',
+                    value: {
+                        event,
+                        path
+                    }
+                })
+            }
+        })
+    })
+}
+
+const subscribeMetabaseState = (events: Array<EventListener>) => {
+    eventListeners.push(...events)
 }
 
 export const rpc = {
