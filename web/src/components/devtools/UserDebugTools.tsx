ba5e3c3aaab3dbf34662d91e39030d42be0c77ff
nuwandavek <vivekaithal44@gmail.com>
2025-07-03 18:15:08 +0530

    works on all pages

--- web/src/components/devtools/UserDebugTools.tsx
+++ web/src/components/devtools/UserDebugTools.tsx
@@ -7,18 +7,18 @@ import { getDashboardAppState } from '../../../../apps/src/metabase/helpers/dash
 const useAppStore = getApp().useStore()
 
 
-const downloadAction = (content: string, name: string) => {
+const downloadState = async (pageType: string) => {
+    const state = await getApp().getState();
+    const content = JSON.stringify(state, null, 2)
     const blob = new Blob([content], { type: "application/json" })
     const url = URL.createObjectURL(blob)
     const a = document.createElement('a')
     a.href = url
-    a.download = name
+    const name = state?.name || `${pageType}_state`
+    a.download = `${name}.json`
     a.click()
     a.remove()
   }
-
-
-
 
 export const UserDebugTools: React.FC = () => {
     const pageType = useAppStore((state) => state.toolContext.pageType)
@@ -26,14 +26,11 @@ export const UserDebugTools: React.FC = () => {
         <VStack mb={4} alignItems={"stretch"} spacing={2} width="100%">
             <Text fontSize="2xl" fontWeight="bold">User Debug Tools</Text>
             <Text > Tools for users to debug and look into Metabase internal workings</Text>
-            { pageType === 'dashboard' &&
-            <SettingsBlock title="Dashboard Debug">
+            { pageType != 'unknown' &&
+            <SettingsBlock title="State Debug">
                 <HStack spacing={2} justifyContent="space-between">
-                <Text >Download Dashboard State</Text>
-                <Button colorScheme='minusxGreen' size="xs" onClick={async () => {
-                    const state = await getDashboardAppState();
-                    downloadAction(JSON.stringify(state, null, 2), 'dashboard.json')
-                }}>Download</Button>
+                <Text >{`Download ${pageType.charAt(0).toUpperCase() + pageType.slice(1)} State`}</Text>
+                <Button colorScheme='minusxGreen' size="xs" onClick={async() => await downloadState(pageType)}>Download</Button>
                 </HStack>
             </SettingsBlock>}
         </VStack>
