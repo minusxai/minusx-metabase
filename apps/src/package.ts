daf6ad9e0f2085fd54c9157ae95fc1ded634836e
Arpit Tarang Saxena <arpit.tarang@gmail.com>
2025-07-04 15:11:06 +0530

    invalidate model info on hard sync (#222)

--- apps/src/package.ts
+++ apps/src/package.ts
@@ -2,6 +2,7 @@ export { getAppStateConfigs } from "./appStateConfigs";
 export { applyTableDiffs } from "./common/utils";
 export { getTableContextYAML, filterTablesByCatalog } from "./metabase/helpers/catalog";
 export { getTableData, getDatabaseTablesAndModelsWithoutFields } from "./metabase/helpers/metabaseAPIHelpers";
+export { fetchModelInfo } from "./metabase/helpers/metabaseAPI";
 export { getAllTemplateTagsInQuery } from "./metabase/helpers/sqlQuery";
 export { getModelsWithFields, getSelectedAndRelevantModels, modifySqlForMetabaseModels, replaceLLMFriendlyIdentifiersInSqlWithModels } from "./metabase/helpers/metabaseModels";
 export { getCurrentQuery, getDashboardState } from "./metabase/helpers/metabaseStateAPI";
