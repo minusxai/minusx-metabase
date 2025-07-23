936adf89e10026483768c573c1fd19b3e517d668
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-07-23 21:02:34 +0530

    Implement embedded auth flow

--- web/src/components/common/App.tsx
+++ web/src/components/common/App.tsx
@@ -149,6 +149,7 @@ const AppLoggedIn = forwardRef((_props, ref) => {
   const analystMode = useSelector((state: RootState) => state.settings.analystMode)
   const drMode = useSelector((state: RootState) => state.settings.drMode)
   const currentAgent: AgentType = analystMode ? AGENTS.EXPLORER : drMode ? AGENTS.SIMPLE : AGENTS.CLASSIC
+  const isEmbedded = getParsedIframeInfo().isEmbedded as unknown === 'true'
 
   const agentIconMap: Record<AgentType, any> = {
     [AGENTS.EXPLORER]: BiSolidMapAlt,
@@ -193,10 +194,22 @@ const AppLoggedIn = forwardRef((_props, ref) => {
   }, [])
 
   useEffect(() => {
-    const isEmbedded = getParsedIframeInfo().isEmbedded as unknown === 'true'
     const checkToken = async () => {
       const mx_token = await getMXToken()
-      console.log('MX Token is', mx_token)
+      
+      if (mx_token) {
+        try {
+          const embedAuthResult = await authModule.embedAuth(mx_token)
+          const { session_jwt, profile_id, email } = embedAuthResult
+          dispatch(login({
+            session_jwt,
+            profile_id,
+            email,
+          }))
+        } catch (error) {
+          console.error('Failed to authenticate embed token:', error)
+        }
+      }
     }
     if (isEmbedded) {
       checkToken()
@@ -225,7 +238,7 @@ const AppLoggedIn = forwardRef((_props, ref) => {
   }, [])
   useEffect(() => {
     const attemptRefresh = () => {
-      if (configs.IS_DEV) {
+      if (configs.IS_DEV || isEmbedded) {
         return
       }
       authModule.refresh().then(({ expired, changed, session_jwt, profile_id, email }) => {
@@ -257,7 +270,6 @@ const AppLoggedIn = forwardRef((_props, ref) => {
 //   const isDevToolsOpen = useSelector((state: RootState) => state.settings.isDevToolsOpen)
   const platformShortcut = getPlatformShortcut()
   const width = getParsedIframeInfo().width
-  const isEmbedded = getParsedIframeInfo().isEmbedded as unknown === 'true'
 
   const clearMessages = () => {
     if (taskInProgress) {
