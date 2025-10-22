3c8d0df6dd426a1fe36422684f549051a90496b0
Sreejith <1743700+ppsreejith@users.noreply.github.com>
2025-10-22 15:59:20 +0530

    Add error message to sql query exec failure

--- apps/src/metabase/helpers/dashboard/runSqlQueryFromDashboard.ts
+++ apps/src/metabase/helpers/dashboard/runSqlQueryFromDashboard.ts
@@ -2,8 +2,17 @@ import { DashboardInfo, DatasetResponse } from "./types";
 import { executeQuery, executeMBQLQuery } from '../metabaseAPIHelpers';
 
 export const runSQLQueryFromDashboard = async (sql: string, databaseId: number, templateTags = {}) => {
-  const response = await executeQuery(sql, databaseId, templateTags) as DatasetResponse;
-  return response
+  try {
+    return await executeQuery(sql, databaseId, templateTags) as DatasetResponse;
+  } catch (error) {
+    let errMessage = error?.response?.message || error.message
+    if (errMessage.includes('403')) {
+      errMessage += " - You do not have permission to run this query.";
+    }
+    return {
+      error: errMessage
+    }
+  }
 }
 
 export const runMBQLQueryFromDashboard = async (mbql: any, databaseId: number) => {
